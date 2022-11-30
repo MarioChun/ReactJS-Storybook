9fa76f1bbab299365e8704db731cc51c10544367
Mario <mario@Marioui-iMac.local>
2022-11-30 19:09:59 +0900

    [component] ipnut 개발중

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,12 +5,13 @@ import "./input.scss";
 //질문 1. 프로퍼티가 많아질수록 App.tsx쪽에 넣어줘야하는 프로퍼티 양이 많아지는데 이부분 개선시키는 방법이 없나요
 type inputProps = {
   placeholder: string;
+  size?: "sm" | "md" | "lg";
 } & InputHTMLAttributes<HTMLInputElement>;
 
-const Input: React.FC<inputProps> = ({ placeholder }) => {
+const Input: React.FC<inputProps> = ({ placeholder, size }) => {
   return (
     <div>
-      <input placeholder={placeholder} />
+      <input placeholder={placeholder} className={`${size}`} />
     </div>
   );
 };
